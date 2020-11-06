13b2dfa0b9f371e7ec42ef9199d0add3997f6702
TheHypnoo <mysterlegendz@gmail.com>
2020-11-06 11:49:22 +0100

    La jungla matematica...

--- app/src/main/java/com/sergigonzalez/lajunglamatematica/Nivel.kt
+++ app/src/main/java/com/sergigonzalez/lajunglamatematica/Nivel.kt
@@ -1,12 +1,25 @@
 package com.sergigonzalez.lajunglamatematica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Nivel : AppCompatActivity() {
+
+    private lateinit var ET_Respuesta: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nivel)
+        InitButtons()
+        initListeners()
     }
 
+    private fun InitButtons() {
+        ET_Respuesta = findViewById(R.id.editTextNumber)
+    }
+    private fun initListeners() {
+//"https://medium.com/mobile-app-development-publication/making-android-edittext-accept-number-only-efbe2ba1cd69"
+    }
 }
