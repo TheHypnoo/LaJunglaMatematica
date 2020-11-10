191d7bed22602960f2e9cf52a977345dca35d473
TheHypnoo <mysterlegendz@gmail.com>
2020-11-10 22:53:22 +0100

    Para torre...! DB en proceso...

--- app/src/main/java/com/sergigonzalez/lajunglamatematica/AuthActivity.kt
+++ app/src/main/java/com/sergigonzalez/lajunglamatematica/AuthActivity.kt
@@ -3,22 +3,35 @@ package com.sergigonzalez.lajunglamatematica
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AuthActivity : AppCompatActivity() {
-
+    private lateinit var Obtener: Button
+    private lateinit var nomUsuari: TextView
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var SignUpButton: Button
     private lateinit var logInButton: Button
+    // Access a Cloud Firestore instance from your Activity
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        nomUsuari = findViewById(R.id.nomUsuari)
+        Obtener = findViewById(R.id.Obtener)
         setup()
+        CrearUsuari()
+        Obtener.setOnClickListener {
+            saberUsuari()
+        }
+
     }
 
     private fun setup(){
@@ -58,6 +71,43 @@ class AuthActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun CrearUsuari(){
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "Nombre" to "Sergi",
+            "Apellido" to "Gonzalez",
+            "Nombre de Usuario" to "TheHypnoo",
+            "Fecha de nacimiento" to 2000
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("DB: Users", "Añadido con el ID => ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("DB: Users", "Error al añadir el usuario", e)
+            }
+    }
+
+    private fun saberUsuari(){
+        val users = db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("", "${document.id} => ${document.data}").toString()
+                    if(document.data.equals("Nombre de Usuario")) {
+                        val usuario = document.data
+                        nomUsuari.setText("$usuario").toString()
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("DB: Users", "Error al intentar obtener la información", exception)
+            }
     }
 
     private fun showAlert() {
--- app/src/main/java/com/sergigonzalez/lajunglamatematica/Ranking.kt
+++ app/src/main/java/com/sergigonzalez/lajunglamatematica/Ranking.kt
@@ -10,32 +10,10 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class Ranking : AppCompatActivity() {
     private lateinit var textViewJugador: TextView
-    // Access a Cloud Firestore instance from your Activity
-    val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
         textViewJugador = findViewById(R.id.textViewJugador)
-        // Create a new user with a first and last name
-        val user = hashMapOf(
-            "first" to "Ada",
-            "last" to "Lovelace",
-            "born" to 1815
-        )
-
-// Add a new document with a generated ID
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-        /*
-        var user = FirebaseAuth.getInstance().currentUser?.email.toString()
-        textViewJugador.setText(user)
-        */
     }
 
 }
