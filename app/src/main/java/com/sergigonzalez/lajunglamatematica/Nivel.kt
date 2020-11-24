20a2438e1b597b46249f6777648016f55d265927
TheHypnoo <mysterlegendz@gmail.com>
2020-11-24 12:03:08 +0100

    Niveles saving? Next!

--- app/src/main/java/com/sergigonzalez/lajunglamatematica/Nivel.kt
+++ app/src/main/java/com/sergigonzalez/lajunglamatematica/Nivel.kt
@@ -1,18 +1,19 @@
 package com.sergigonzalez.lajunglamatematica
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.airbnb.lottie.LottieAnimationView
-import kotlinx.android.synthetic.main.activity_nivel.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Nivel : AppCompatActivity() {
     private lateinit var queHacer: TextView
@@ -21,19 +22,30 @@ class Nivel : AppCompatActivity() {
     private lateinit var Incorrecto: LottieAnimationView
     private lateinit var ResultadoEditText: EditText
     private lateinit var bt_corregir: Button
-    var numero1: Int = 0
-    var numero2: Int = 0
-    var Resultado: Int = 0
+    val db = FirebaseFirestore.getInstance()
+    var id = ""
+    private var numero1 = 0
+    private var numero2 = 0
+    private var Resultado = 0
+    private var posNivel = -1
+    private var x = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_nivel)
-        FindID()
-        Nivel1Suma()
+        findID()
+        whenNiveles()
+        nivelSuma()
     }
 
-    private fun FindID(){
+    override fun onStart() {
+        buscaNivel()
+        super.onStart()
+
+    }
+
+    private fun findID(){
         queHacer = findViewById(R.id.queHacer)
         enunciadoNivel = findViewById(R.id.enunciadoNivel)
         Correcto = findViewById(R.id.Correcto)
@@ -42,49 +54,60 @@ class Nivel : AppCompatActivity() {
         bt_corregir = findViewById(R.id.bt_corregir)
     }
 
+    private fun buscaNivel(): Int {
+        val user = FirebaseAuth.getInstance().currentUser
+        val email = user?.email
+        db.collection("users")
+                .whereEqualTo("Email", email)
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        Log.d("TAG", "${document.id} => ${document.data}")
+                        id = document.id
+                        posNivel = document.data["queNivel"].toString().toLong().toInt()
+                        println("InfoNivel: $posNivel")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("TAG", "Error getting documents: ", exception)
+                }
+        return posNivel
+    }
+
     @SuppressLint("SetTextI18n")
-    private fun Nivel1Suma(){
-        numero1 = generaNumeros()
-        numero2 = generaNumeros()
-        queHacer.text = "Suma los dos numeros y escribe el resultado abajo."
-        enunciadoNivel.text =
-                " ¡Hola amigo! Necesito tu ayuda." +
-                " Mi mama me ha pedido que vaya a comprar fruta al mercado " +
-                "y me ha pedido que compre " +numero1+ " manzanas  y " +numero2+ " peras. " +
-                "¿Cuantas piezas de fruta  tengo que comprar en total?"
+    private fun whenNiveles(){
+        println("Level: $posNivel")
+        if(posNivel >= x) {
+            x = posNivel
 
-        Resultado = numero1 + numero2
-        bt_corregir.setOnClickListener{
-            if(ResultadoEditText.text.toString() == Resultado.toString()) {
-                Correcto.visibility = View.VISIBLE
-                Correcto.playAnimation()
-            } else {
-                Incorrecto.visibility = View.VISIBLE
-                Incorrecto.playAnimation()
+        when (x) {
+            0 -> {
+                enunciadoNivel.text =
+                        " ¡Hola amigo! Necesito tu ayuda." +
+                                " Mi mama me ha pedido que vaya a comprar fruta al mercado " +
+                                "y me ha pedido que compre " +numero1+ " manzanas  y " +numero2+ " peras. " +
+                                "¿Cuantas piezas de fruta  tengo que comprar en total?"
             }
-
-            bt_corregir.visibility = View.GONE
-            Handler(Looper.getMainLooper()).postDelayed({
-                Nivel2Suma()
-                Incorrecto.visibility = View.GONE
-                Correcto.visibility = View.GONE
-                bt_corregir.visibility = View.VISIBLE
-                ResultadoEditText.text.clear()
-            }, 3000)
-
+            1 -> {
+                enunciadoNivel.text =
+                        "A cambio de comprarle la fruta, mi mama me dio " +
+                                numero1 + " euros así que los fui a meter en mi hucha. A más, yo ya tenía en mi hucha " +
+                                numero2 + " euros. ¿Cuánto dinero tengo ahora en la hucha?"
+                }
+            2 -> {
+                enunciadoNivel.text =
+                        "Y este es el puto lvl3"
+            }
+            }
         }
     }
 
     @SuppressLint("SetTextI18n")
-    private fun Nivel2Suma(){
+    private fun nivelSuma(){
         numero1 = generaNumeros()
         numero2 = generaNumeros()
+        whenNiveles()
         queHacer.text = "Suma los dos numeros y escribe el resultado abajo."
-        enunciadoNivel.text =
-                "A cambio de comprarle la fruta, mi mama me dio "+
-                        numero1+ " euros así que los fui a meter en mi hucha. A más, yo ya tenía en mi hucha " +
-        numero2+ " euros. ¿Cuánto dinero tengo ahora en la hucha?"
-
         Resultado = numero1 + numero2
         bt_corregir.setOnClickListener{
             if(ResultadoEditText.text.toString() == Resultado.toString()) {
@@ -94,13 +117,16 @@ class Nivel : AppCompatActivity() {
                 Incorrecto.visibility = View.VISIBLE
                 Incorrecto.playAnimation()
             }
-
             bt_corregir.visibility = View.GONE
+
             Handler(Looper.getMainLooper()).postDelayed({
-                val mainIntent = Intent(this, menuPrincipal::class.java)
-                startActivity(mainIntent)
-                finish()
-            }, 4000)
+                ++x
+                Incorrecto.visibility = View.GONE
+                Correcto.visibility = View.GONE
+                bt_corregir.visibility = View.VISIBLE
+                ResultadoEditText.text.clear()
+                nivelSuma()
+            }, 3000)
 
         }
     }
